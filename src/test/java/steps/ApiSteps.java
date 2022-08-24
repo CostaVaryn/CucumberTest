4916098588ce85acda53c68c068ac0f1d86e5c0e
Costa Varyn <d3tug@yandex.ru>
2022-08-24 15:54:15 +0300

    add ApiSteps.java

---
+++ src/test/java/steps/ApiSteps.java
@@ -0,0 +1,30 @@
+package steps;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
+
+public class ApiSteps {
+    private String var;
+    public String getVar() {
+        return var;
+    }
+
+    @When("^отправли запрос на ресурс \"([^\"]*)\"$")
+    public void sendRequest(String url) {
+        Selenide.open(url);
+        var = String.valueOf(RestAssured.given().get(url).statusCode());
+    }
+
+    @When("^проверили, значение переменной равно 200$")
+    public void checkStatus200() {
+        Assertions.assertEquals("200", getVar());
+    }
+
+    @When("^проверили, значение переменной равно 400$")
+    public void checkStatus400() {
+        Assertions.assertEquals("400", getVar());
+    }
+}
